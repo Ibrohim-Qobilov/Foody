d11239efea847dc7fa4a7b358ce0b0c711daa499
ibrokhim-kobilov <ibrokhim112@gmail.com>
2022-01-18 20:19:30 +0500

    added color list

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors{
+  static Color greenColor = const Color(0xff22A45D);
+  static Color white10Color = const Color(0xff22A45D);
+  static Color whiteColor = const Color(0xffffffff);
+  static Color greyColor = const Color(0xff868686);
+  static Color orengeColor = const Color(0xffEF9920);
+  static Color blueColor = const Color(0xff4285F4);
+  static Color deepBlueColor = const Color(0xff395998);
+  
+}
